e43a1867c43ac5ab986b8c4c67eb23558c4fc9f5
GreenJuzzy <65908209+GreenJuzzy@users.noreply.github.com>
2022-03-26 22:19:45 +0100

    New Movements

--- index.js
+++ index.js
@@ -3,9 +3,30 @@ var fs = require("fs");
 var process = require("process");
 
 // Definitions
-var map = fs.readFileSync(__dirname + "/map.txt", "utf-8").split("")
+var map = fs.readFileSync(__dirname + "/map.txt", "utf-8")
+
+const settings = {
+    character: "*",
+    borders: ["#"]
+}
 
 // Functions
+
+/**
+ * @param {String[]} map The full-sized map.
+ */
+
+var formatMap = async (map) => {
+    var newArr = []
+
+    map = map.replaceAll("\r", "").replaceAll(" ", "").split("\n").forEach((value, index, array) => {
+        newArr[index] = value.split("")
+    })
+
+    newArr.forEach((value, index, array) => { if (value[value.length - 1] == "") value.pop() })
+
+    return newArr
+}
 
 /**
  * 
@@ -19,7 +40,49 @@ var map = fs.readFileSync(__dirname + "/map.txt", "utf-8").split("")
  */
 
 var move = async (direction, render, player, map) => {
-    if (!map.includes(player)) return { error: "No character found." }
+    var newMap
 
+
+    console.log({ map })
+    if (!map.join("").includes(settings.character)) return { error: "No character found." }
+    
+    var rowMap = {
+        row: () => {
+            var result = []
+            map.filter((value, index, array) => {
+                if(value.includes(settings.character)) result = value
+            })
+            return result.length > 0 ? result : map
+        },
+        left: () => {
+
+        },
+        above: () => {
+            var result = []
+            map.forEach((value, index, array) => {
+                if (value.includes(settings.character)) { result = map[index - 1] }
+            })
+            return result || undefined
+        },
+        under: () => {
+            var result = []
+            map.forEach((value, index, array) => {
+                if (value.includes(settings.character)) { result = map[index + 1] }
+            })
+            return result || undefined
+        }
+    }
+
+    switch (direction) {
+        case "up":
+            newMap = rowMap.above()
+            break;
+    
+        default:
+            break;
+    }
 }
+(async () => {
+    move("up", 3, "*", await formatMap(map))
+})()
 
